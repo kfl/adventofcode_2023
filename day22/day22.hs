ab135e5674b29fe411848ec996aea648150a2d5f
Ken Friis Larsen <ken@friislarsen.net>
2023-12-23 11:52:21 +0100

    Improve debugging notes for day 22

--- day22/day22.hs
+++ day22/day22.hs
@@ -7,12 +7,11 @@ import qualified Data.Map.Strict as Map
 import Data.Map.Strict (Map)
 import qualified Data.Set as Set
 import Data.Set (Set)
-
-
 import Data.Function (on)
 
 import Text.Printf
 import System.Process (callCommand)
+import Control.Exception (assert)
 
 
 test =  map parse [ "1,0,1~1,2,1"
@@ -23,7 +22,8 @@ test =  map parse [ "1,0,1~1,2,1"
                   , "0,1,6~2,1,6"
                   , "1,1,8~1,1,9"
                   , "1,1,11~1,1,12"  -- Extra brick for debugging
-                  , "1,0,15~1,0,16"  -- Extra brick for debugging
+                  , "1,0,10~1,2,10"  -- Extra brick for debugging
+                  , "1,0,18~1,0,19"  -- Extra brick for debugging
                   ]
 input = map parse . lines <$> readFile "input.txt"
 
@@ -53,6 +53,7 @@ showCuboids cuboids = do
   callCommand "./viewcuboids.py"
 showBricks bricks = showCuboids $ map brickToCuboid bricks
 
+
 rectOverlap (C{x=x1,y=y1,w=w1,h=h1}) (C{x=x2,y=y2,w=w2,h=h2}) =
   x1 < x2 + w2 && x2 < x1 + w1 && y1 < y2 + h2 && y2 < y1 + h1
 
@@ -63,10 +64,22 @@ oneFall placed c = over ++ c' : under
 downFall cuboids = L.foldl' oneFall [] $ L.sortOn z cuboids
 
 
+cuboidsOverlap (C x1 y1 z1 w1 h1 d1) (C x2 y2 z2 w2 h2 d2) =
+    x1 < x2 + w2 && x2 < x1 + w1 && y1 < y2 + h2 && y2 < y1 + h1
+    && z1 < z2 + d2 && z2 < z1 + d1
+
+overlappingCuboids cuboids = [(c1, c2) | (c1:cs) <- L.tails cuboids, c2 <- cs
+                                       , cuboidsOverlap c1 c2]
+
 --part1 :: Input -> Int
 part1 input = length cuboids - Set.size essentials
   where
-    cuboids = downFall $ map brickToCuboid input
+    cuboids' = downFall $ map brickToCuboid input
+
+    -- FIXME: For future debugging this assertion fail.
+    --        Thus, there is a bug in downFall
+    cuboids = assert (null $ overlappingCuboids cuboids') cuboids'
+
     zmap = Map.fromListWith (++) [ (top, [c]) | c@C{z, d} <- cuboids, let top = z+d ]
     potentials z = Map.findWithDefault [] z zmap
     supporters = Map.fromListWith (++) [ (c, [p]) | c@C{z} <- cuboids
@@ -76,8 +89,6 @@ part1 input = length cuboids - Set.size essentials
     cset = Set.fromList cuboids
     safe = Set.difference cset essentials
     freeloaders = Set.difference cset $ Set.fromList $ concat $ Map.elems supporters
-    -- FIXME: For future debugging this invariant does not hold: length input == Set.size cset
-    --        There is properly a bug in downFall
 
 answer1 = part1 <$> input
 
