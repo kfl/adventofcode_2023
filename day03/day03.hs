5f3268573d7709695d6ef45814f25e58963f0902
Ken Friis Larsen <ken@friislarsen.net>
2023-12-27 10:46:11 +0100

    Minor clean up

--- day03/day03.hs
+++ day03/day03.hs
@@ -34,28 +34,28 @@ parse :: [String] -> Input
 parse inp = sch
   where
     (_, sch) = L.foldl' (\(i, sch) r ->
-                           let (_, j, sch', js, cur) = L.foldl' row (i, 0, sch, [], 0) r
+                           let (_, sch', js, cur) = L.foldl' (row i) (0, sch, [], 0) r
                            in (i+1, insertNum i cur sch' js))
                         (0, Map.empty) inp
 
     insertNum i num sch' js =
       foldr (\j sch -> Map.insert (i, j) (Number num (i,js)) sch) sch' js
 
-    row (i, j, sch, js, cur) c =
-      if C.isDigit c then (i, j+1, sch, j:js, cur * 10 + C.digitToInt c)
+    row i (j, sch, js, cur) c =
+      if C.isDigit c then (j+1, sch, j:js, cur * 10 + C.digitToInt c)
       else let sch1 = insertNum i cur sch js
                sch2 = if c == '.' then sch1
                       else Map.insert (i, j) (Symbol c) sch1
-            in (i, j+1, sch2, [], 0)
+            in (j+1, sch2, [], 0)
 
 neighbours (i, j) = [(i-1, j-1), (i-1, j), (i-1, j+1),
                      (i  , j-1)          , (i  , j+1),
                      (i+1, j-1), (i+1, j), (i+1, j+1)]
 
 nextToSymbol sch = Set.fromList nexts
-  where syms = [ s | (s, Symbol _) <- Map.assocs sch]
-        nexts = [ n | s <- syms, k <- neighbours s,
-                  n@(Number _ _) <- toList $ Map.lookup k sch]
+  where nexts = [ n | (s, Symbol _) <- Map.assocs sch
+                    , k <- neighbours s
+                    , n@(Number _ _) <- toList $ Map.lookup k sch]
 
 
 part1 :: Input -> Int
@@ -64,11 +64,10 @@ part1 input = sum [ n | Number n _ <- nexts ]
 answer1 = part1 <$> input
 
 gearRatios sch = ratios
-  where syms = [ s | (s, Symbol '*') <- Map.assocs sch]
-        ratios = [ product [ n | Number n _ <- toList gs]
-                 | s <- syms
+  where ratios = [ product [ n | Number n _ <- toList gs]
+                 | (s, Symbol '*') <- Map.assocs sch
                  , let gs = Set.fromList [ n | k <- neighbours s
-                                         , n@(Number _ _) <- toList $ Map.lookup k sch]
+                                             , n@(Number _ _) <- toList $ Map.lookup k sch]
                  , Set.size gs == 2 ]
 
 part2 :: Input -> Int
